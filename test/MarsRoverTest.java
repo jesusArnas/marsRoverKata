115dfe0899106515d3caed6ccd4b401bc223e713
jesusArnas <jesus.arnas@gmail.com>
2018-05-03 19:51:49 +0200

    Initial commit

---
+++ test/MarsRoverTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MarsRoverTest {
+
+    @Test
+    public void firstTest()
+    {
+        MarsRover mr = new MarsRover();
+        assertNull(mr);
+    }
+}
