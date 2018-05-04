59f746decf1d3f009e7ed32a278acabaff34c7ab
jesusArnas <jesus.arnas@gmail.com>
2018-05-04 17:38:25 +0200

    Creado constructor del rover

--- src/MarsRover.java
+++ src/MarsRover.java
@@ -1,3 +1,12 @@
 public class MarsRover {
 
+    private String direction;
+
+    public MarsRover(int x, int y, String direction) {
+        this.direction = direction;
+    }
+
+    public String getDirection() {
+        return this.direction;
+    }
 }
--- test/MarsRoverTest.java
+++ test/MarsRoverTest.java
@@ -5,9 +5,17 @@ import static org.junit.jupiter.api.Assertions.*;
 public class MarsRoverTest {
 
     @Test
-    public void firstTest()
+    public void roverInitialization()
     {
-        MarsRover mr = new MarsRover();
-        assertNull(mr);
+        MarsRover mr = new MarsRover(1,2,"N");
+        assertNotNull(mr);
+    }
+
+    @Test
+    public void knownIsDirection() {
+        MarsRover mr = new MarsRover(1,2,"N");
+        assertEquals("N", mr.getDirection());
+        mr = new MarsRover(1,2,"S");
+        assertEquals("S", mr.getDirection());
     }
 }
